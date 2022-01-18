3a6d7aefd2983928d5a1c7cc4b1a3c9f40d8a3e3
Denys Boiko <denys.boiko@gmail.com>
2022-01-18 14:44:24 +0200

    Create Salary and Income components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SalaryComponent} from './salary/salary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'salary/', component: SalaryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SalaryComponent } from './salary/salary.component';
+import { IncomeComponent } from './income/income.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalaryComponent,
+    IncomeComponent
   ],
   imports: [
     BrowserModule,
